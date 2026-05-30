a8cde1c825d33ff32b1fceec0134b8598321cd5d
VitorKavaharada <mailprojweb@gmail.com>
2026-05-30 10:54:40 -0300

    Update todo_model.dart

--- lib/models/todo_model.dart
+++ lib/models/todo_model.dart
@@ -1,12 +1,14 @@
 class TodoModel {
   String id;
   String titulo;
+  String estimativa;
   bool pronto;
   String userId;
 
   TodoModel({
     required this.id,
     required this.titulo,
+    required this.estimativa,
     this.pronto = false,
     required this.userId,
   });
@@ -15,6 +17,7 @@ class TodoModel {
     return TodoModel(
       id: id,
       titulo: map['titulo'] ?? '',
+      estimativa: map['estimativa'] ?? 'Não informada',
       pronto: map['pronto'] ?? false,
       userId: map['userId'] ?? '',
     );
@@ -23,6 +26,7 @@ class TodoModel {
   Map<String, dynamic> toMap() {
     return {
       'titulo': titulo,
+      'estimativa': estimativa,
       'pronto': pronto,
       'userId': userId,
     };
